54f926a183b58c1746a571fe87012830b8e1f5ee
jpassgo <jeffpascoe9@gmail.com>
2020-12-15 18:57:40 -0600

    Rewriting Posting and AddPosting. Planning on using a connector instead.

--- junk-trader/src/components/Posting.tsx
+++ junk-trader/src/components/Posting.tsx
@@ -1,27 +1,32 @@
-import * as React from "react"
-import { Dispatch } from "redux"
-import { useDispatch } from "react-redux"
+import React, { Component } from 'react';
+import { Card, Theme, WithStyles, withStyles } from '@material-ui/core';
+import CardContent from '@material-ui/core/CardContent'
+import { render } from '@testing-library/react';
+import Grid from '@material-ui/core/Grid';
+import { Styles, StyledComponentProps } from '@material-ui/styles';
 
-type Props = {
-    posting: Posting
-    removePosting: (posting: Posting) => void
+const styles: Styles<Theme, StyledComponentProps> = (theme) => ({
+    
+})
+
+class Posting extends Component<any>{
+    render(): JSX.Element {
+        return (
+            <Card>
+                <CardContent>
+                    <Grid container spacing={2} justify={'center'}>
+                        <Grid item xs={10}>
+                            <p>Title</p>
+                            <p>Price</p>
+                            <p>Description</p>    
+                        </Grid>
+                    </Grid>
+                </CardContent>
+            </Card>
+        )
+    }
 }
 
-export const Posting: React.FC<Props> = ({ posting, removePosting }) => {
-    const dispatch: Dispatch<any> = useDispatch()
+export type PostingProps = WithStyles<typeof styles>
 
-    const deletePosting = React.useCallback(
-        (posting: Posting) => dispatch(removePosting(posting)),
-        [dispatch, removePosting]
-    )
-
-    return(
-        <div className="Posting">
-            <div>
-                <h3>{posting.title}</h3>
-                <p>{posting.price}</p>
-                <p>{posting.description}</p>
-            </div>
-        </div>
-    )
-}
+export default withStyles(styles, {withTheme: true})(Posting)
