b256569ede3a7124ece9ac6c0133f98a9f77135a
jpassgo <jeffpascoe9@gmail.com>
2020-12-17 18:56:55 -0600

    Copying Posting into AddPosting. Need to update appropriately

---
+++ junk-trader/src/components/AddPosting.tsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { Card, Theme, WithStyles, withStyles } from '@material-ui/core';
+import CardContent from '@material-ui/core/CardContent'
+import { render } from '@testing-library/react';
+import Grid from '@material-ui/core/Grid';
+import { Styles, StyledComponentProps } from '@material-ui/styles';
+
+const styles: Styles<Theme, StyledComponentProps> = (theme) => ({
+    
+})
+
+export class AddPosting extends Component<any>{
+    render(): JSX.Element {
+        return (
+            <Card>
+                <CardContent>
+                    <Grid container spacing={2} justify={'center'}>
+                        <Grid item xs={10}>
+                            <p>Title</p>
+                            <p>Price</p>
+                            <p>Description</p>    
+                        </Grid>
+                    </Grid>
+                </CardContent>
+            </Card>
+        )
+    }
+}
+
+export type AddPostingProps = WithStyles<typeof styles>
+
+export default withStyles(styles, {withTheme: true})(AddPosting)
