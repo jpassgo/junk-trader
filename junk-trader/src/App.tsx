123cf103f017f636a4213d9c9dfad33378f8cf8b
jpassgo <jeffpascoe9@gmail.com>
2020-12-17 18:56:30 -0600

    Temporarily commenting out AddPosting.

--- junk-trader/src/App.tsx
+++ junk-trader/src/App.tsx
@@ -3,8 +3,8 @@ import { useSelector, shallowEqual, useDispatch } from "react-redux"
 import "./App.css"
 
 import { Posting } from "./components/Posting"
-import { AddPosting } from "./components/AddPosting"
-import { addPosting, removePosting } from "./store/actionCreators"
+// import { AddPosting } from "./components/AddPosting"
+// import { addPosting, removePosting } from "./store/actionCreators"
 import { Dispatch } from "redux"
 
 const App: React.FC = () => {
@@ -16,7 +16,7 @@ const App: React.FC = () => {
   return (
     <main>
         <h1>My Articles</h1>
-        <AddPosting savePosting={savePosting} />
+        {/* <AddPosting savePosting={savePosting} /> */}
         {postings.map((posting: Posting) => (
             <Posting
                 key={posting.id}
