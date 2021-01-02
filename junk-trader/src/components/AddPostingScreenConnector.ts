538ca35b2bac7da83b10011e5fdf29581891fd51
jpassgo <jeffpascoe9@gmail.com>
2021-01-02 15:39:24 -0600

    Creating a function that will dispatch an action to add a posting to the posting list.

--- junk-trader/src/components/AddPostingScreenConnector.ts
+++ junk-trader/src/components/AddPostingScreenConnector.ts
@@ -1,14 +1,28 @@
+import { AccordionActions } from '@material-ui/core'
+import actions from '../store/actions'
 import { connect } from 'react-redux'
+import { AnyAction, Dispatch } from 'redux'
 import AddPostingScreen, {
     AddPostingScreenProps
 } from './AddPostingScreen'
+
+
+const addPostingToMarketplace = (posting: Posting): AnyAction => {
+    return {
+        type: actions.ADD_POSTING,
+        postingToAdd: posting
+    }
+}
 
 const mapStateToProps = (): AddPostingScreenProps => {
     return ({} as unknown) as AddPostingScreenProps;
 }
 
-const mapDispatchToProps = (): AddPostingScreenProps => {
-    return (({} as unknown) as AddPostingScreenProps);
-}
+const mapDispatchToProps = (dispatch: Dispatch): AddPostingScreenProps => 
+    (({
+        addPosting: (posting: Posting) => {
+            dispatch(addPostingToMarketplace(posting));
+        },
+    } as unknown) as AddPostingScreenProps);
 
 export default connect(mapStateToProps, mapDispatchToProps)(AddPostingScreen);
