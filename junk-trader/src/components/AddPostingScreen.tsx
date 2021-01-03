43d195d174d6c99f2596973965c1d6638b0c69c4
jpassgo <jeffpascoe9@gmail.com>
2021-01-03 13:56:27 -0600

    Updaing the add posting screen to take in a posting

--- junk-trader/src/components/AddPostingScreen.tsx
+++ junk-trader/src/components/AddPostingScreen.tsx
@@ -7,7 +7,7 @@ import Grid from '@material-ui/core/Grid';
 
 const styles: Styles<Theme, StyledComponentProps> = (theme) => ({
     
-})
+})  
 
 
 export class AddPostingScreen extends Component<any>{
@@ -17,24 +17,37 @@ export class AddPostingScreen extends Component<any>{
     }
 
     render(): JSX.Element {
+        debugger;
+
         return (
            <Grid container spacing={3}>
                <Grid item xs={12}>
-                   <TextField id="outlined-basic" label="Title" variant="outlined" />
+                   <TextField id="title" label="Title" variant="outlined" />
                </Grid>
                <Grid item xs={12}>
-                   <TextField id="outlined-basic" label="Price" variant="outlined" />
+                   <TextField id="price" label="Price" variant="outlined" />
                </Grid>
                <Grid item xs={12}>
-                   <TextField id="outlined-basic" label="Description" variant="outlined" />
+                   <TextField id="description" label="Description" variant="outlined" />
                </Grid>
                <Grid item xs={12}>
-                   <Button variant="outlined" onClick={() => { this.addPosting.bind(this) }}>Submit</Button>
+                   <Button variant="outlined" onClick={() => { this.addPosting.bind({
+                       id: getRandomInt(1, 1000000),
+                       title: this.props.title,
+                       price: this.props.price,
+                       description: this.props.description
+                   }) }}>Submit</Button>
                </Grid>
            </Grid>
         )
     }
 }
+
+function getRandomInt(min: number, max: number) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+  }
 
 export type AddPostingScreenProps = WithStyles<typeof styles>
 
