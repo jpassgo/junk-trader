91005cf1a9802cd71428c3bcaec9fff3b3e91e9d
jpassgo <jeffpascoe9@gmail.com>
2021-01-07 12:53:22 -0600

    Fixing index.tsx so the app will actually start.

--- junk-trader/src/index.tsx
+++ junk-trader/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { createStore, applyMiddleware, Store} from "redux"
 import { Provider } from "react-redux"
 import reducer from "./store/reducer"
-import { render } from 'react-dom'
+import ReactDOM, { render } from 'react-dom'
 import thunk from "redux-thunk"
 import App from './App';
 import './index.css';
@@ -11,11 +11,15 @@ const store: Store<PostingState, PostingAction> & {
   dispatch: DispatchType
 } = createStore(reducer, applyMiddleware(thunk))
 
-const rootElement = document.getElementById("root")
+const rootElement = document.getElementById("root") 
 
-render(
-  <Provider store={store}>
-    <App/>
-  </Provider>,
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App></App>
+    </Provider>
+  </React.StrictMode>,
   rootElement
-)
+);
+
+// ServiceWorker.unregister()
