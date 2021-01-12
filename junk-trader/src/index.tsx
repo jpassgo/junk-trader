3123bf6c68ff63e1a66ea2bb0ddbd3aa9fbf1b0b
jpassgo <jeffpascoe9@gmail.com>
2021-01-11 18:55:59 -0600

    Updating index.tsx to be root of the project.

--- junk-trader/src/index.tsx
+++ junk-trader/src/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { createStore, applyMiddleware, Store} from "redux"
 import { Provider } from "react-redux"
-import reducer from "./store/reducer"
+import reducer from "./reducers/reducer"
 import ReactDOM, { render } from 'react-dom'
 import thunk from "redux-thunk"
 import App from './App';
 import './index.css';
+import AddPostScreen from './components/AddPostScreen';
+import PostsList from './components/PostsList';
 
-const store: Store<PostingState, PostingAction> & {
+const store: Store<PostState, PostAction> & {
   dispatch: DispatchType
 } = createStore(reducer, applyMiddleware(thunk))
 
@@ -16,7 +18,11 @@ const rootElement = document.getElementById("root")
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App></App>
+      <main>
+          <h1>Marketplace</h1>
+          <AddPostScreen/>
+          <PostsList/>
+      </main>
     </Provider>
   </React.StrictMode>,
   rootElement
