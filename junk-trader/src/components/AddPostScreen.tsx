243521b73be8285234700fb6cc4493ee324acb54
jpassgo <jeffpascoe9@gmail.com>
2021-01-14 19:43:45 -0600

    Cleaning up AddPostScreen. Adding props interface. Removing some logic that doesn't quite fit.

--- junk-trader/src/components/AddPostScreen.tsx
+++ junk-trader/src/components/AddPostScreen.tsx
@@ -1,78 +1,51 @@
 import React, { Component } from 'react';
 import { Theme, WithStyles, withStyles } from '@material-ui/core';
 import { Styles, StyledComponentProps } from '@material-ui/styles';
-import { PostScreenProps } from './PostsScreen';
-import { AddPostScreenProps } from './AddPostScreenConnector';
+// import { AddPostScreenProps } from './AddPostScreenConnector';
 import { ReactComponent } from '*.svg';
 
-const styles: Styles<Theme, StyledComponentProps> = (theme) => ({
-    
-})  
+const styles: Styles<Theme, StyledComponentProps> = (theme) => ({});  
 
 
-export class AddPostScreen extends React.Component<AddPostScreenProps>{
+export class AddPostScreen extends React.Component<AddPostScreenProps> {
 
-    addPost(event: any): void {
-        console.log(event)
-        this.addPost(this.state);
+    addPost(post: Post): void {    
     }
 
-    constructor(props: AddPostScreenProps | Readonly<AddPostScreenProps>) {
-        super(props);
+    addPosting(e: any) {
+        e.preventDefault();
+        
+    }
+    // handleChange = (event: { target: { name: any; value: any; }; }) => {
+    //     let nam = event.target.name;
+    //     let val = event.target.value;
+    //     this.setState({[nam]: val});
+    // }
+
+    render(): JSX.Element {
+
         const {
             posts
-        } = props
-
-        this.state = posts.concat({
-            id: getRandomInt(1,100),
-            title: '',
-            price: 0,
-            description: ''
-        })
-
-        this.handleChange = this.handleChange.bind(this);
-        this.addPost = this.addPost.bind(this);
-      }
-    
-      handleChange = (event: { target: { name: any; value: any; }; }) => {
-        let nam = event.target.name;
-        let val = event.target.value;
-        this.setState({[nam]: val});
-      }
-
-    render() {
-        debugger;
+        } = this.props;
 
         return (
-            <form onSubmit={this.addPost}>
-                <label>
-                    Title:
-                    <input type="text" name="title" />
-                </label>
-                <label>
-                    Price:
-                    <input type="text" name="price" />
-                </label>
-                <label>
-                    Description:
-                    <input type="text" name="description" />
-                </label>
-                <input type="submit" value="Submit" />
-            </form>
-        //    <Grid container spacing={3}>
-        //        <Grid item xs={12}>
-        //            <TextField id="title" label="Title" variant="outlined" />
-        //        </Grid>
-        //        <Grid item xs={12}>
-        //            <TextField id="price" label="Price" variant="outlined" />
-        //        </Grid>
-        //        <Grid item xs={12}>
-        //            <TextField id="description" label="Description" variant="outlined" />
-        //        </Grid>
-        //        <Grid item xs={12}>
-        //            <Button variant="outlined" onClick={() => { this.addPost.bind(this) } }>Submit</Button>
-        //        </Grid>
-        //    </Grid>
+            <React.Fragment>
+                <form onSubmit={(e) => {this.addPosting(e)}} className="form-inline">
+                    <label>
+                        Title:
+                        <input type="text" name="title" />
+                    </label>
+                    <label>
+                        Price:
+                        <input type="text" name="price" />
+                    </label>
+                    <label>
+                        Description:
+                        <input type="text" name="description" />
+                    </label>
+                    <button type="submit" value="Submit" />
+                </form>
+            </React.Fragment>  
         );
     }
 }
@@ -81,6 +54,10 @@ function getRandomInt(min: number, max: number) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+}
+
+export interface AddPostScreenProps extends WithStyles<typeof styles> {
+    posts?: Post[];
 }
 
 export default withStyles(styles, {withTheme: true})(AddPostScreen)
