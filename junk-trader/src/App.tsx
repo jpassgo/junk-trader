291a1df2c9a86ac2a016796a778c4e37e8ad5bef
jpassgo <jeffpascoe9@gmail.com>
2021-01-14 19:44:00 -0600

    Adding props to App.

--- junk-trader/src/App.tsx
+++ junk-trader/src/App.tsx
@@ -10,23 +10,32 @@ import reducer from "./reducers/reducer"
 import "./App.css"
 import PostsList from "./components/PostsList"
 import { withStyles, Theme } from "@material-ui/core"
-import { Styles, StyledComponentProps } from "@material-ui/core/styles/withStyles"
+import { Styles, StyledComponentProps, WithStyles } from "@material-ui/core/styles/withStyles"
 import { Component } from "react"
 import thunk from "redux-thunk"
+
+const styles: Styles<Theme, StyledComponentProps> = () => ({})
 
 const store: redux.Store<PostState, PostAction> & {
   dispatch: DispatchType
 } = redux.createStore(reducer, redux.applyMiddleware(thunk))
 
-class App extends Component {
-  render() {
+class App extends Component<AppProps> {
+  render(): JSX.Element {
+    const {
+      posts
+    } = this.props;
+
     return (
       <Provider store={store}>
-        <AddPostScreen/>
+        <AddPostScreen posts={posts}/>
         <PostsList/>
       </Provider>
     );
   }
 }
 
+export interface AppProps extends WithStyles<typeof styles> {
+  posts?: Post[];
+}
 export default App;
