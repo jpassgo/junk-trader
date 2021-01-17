8711b9adbefed7ebb34daa0be7b702e137951ac5
jpassgo <jeffpascoe9@gmail.com>
2021-01-17 15:28:00 -0600

    Removing various unneeded variables.

--- junk-trader/src/components/AddPostScreen.tsx
+++ junk-trader/src/components/AddPostScreen.tsx
@@ -1,48 +1,34 @@
 import React from 'react';
 import { Styles, StyledComponentProps } from '@material-ui/styles';
-import { AnyAction } from 'redux';
-import actions from '../store/actions';
-import { State } from '../store/store';
-import { connect } from 'react-redux';
-import { WithStyles, Theme } from '@material-ui/core'
-import { Dispatch } from 'redux'
+import { Theme, WithStyles, withStyles } from '@material-ui/core'
 
 const styles: Styles<Theme, StyledComponentProps> = (theme) => ({});  
 
+export const AddPostScreen: React.FC<AddPostScreenProps> = props =>  {
+    
+    const { addPost } = props;
 
-export class AddPostScreen extends React.Component<AddPostScreenProps> {
+    const handleClick = () => {
+        addPost({id: getRandomInt(1, 1000), title: "", price: 100.00, description: "" });
+    };
 
-        render(): JSX.Element {
-
-        const {
-            posts,
-            addPost
-        } = this.props;
-
-        const handleClick = () => {
-            addPost({id: getRandomInt(1, 1000), title: "", price: 100.00, description: "" });
-          };
-
-        return (
-            <React.Fragment>
-                <form onSubmit={ handleClick } className="form-inline">
-                    <label>
-                        Title:
-                        <input type="text" name="title" />
-                    </label>
-                    <label>
-                        Price:
-                        <input type="text" name="price" />
-                    </label>
-                    <label>
-                        Description:
-                        <input type="text" name="description" />
-                    </label>
-                    <button type="submit" value="Submit" />
-                </form>
-            </React.Fragment>  
-        );
-    }
+    return (
+        <form onSubmit={ handleClick } className="form-inline">
+            <label>
+                Title:
+                <input type="text" name="title" />
+            </label>
+            <label>
+                Price:
+                <input type="text" name="price" />
+            </label>
+            <label>
+                Description:
+                <input type="text" name="description" />
+            </label>
+            <button type="submit" value="Submit" />
+        </form>
+    );
 }
 
 function getRandomInt(min: number, max: number) {
@@ -52,29 +38,7 @@ function getRandomInt(min: number, max: number) {
 }
 
 export interface AddPostScreenProps extends WithStyles<typeof styles> {
-    posts?: Post[];
     addPost: (post: Post) => void;
 }
 
-const addPostToMarketplace = (post: Post): AnyAction => {
-    return {
-        type: actions.ADD_POST,
-        postToAdd: post
-    }
-}
-
-const mapStateToProps = (state: State): AddPostScreenProps => {
-    return ({
-        posts: state.applicationState.posts
-    } as unknown) as AddPostScreenProps;
-}
-
-const mapDispatchToProps = (dispatch: Dispatch): AddPostScreenProps => 
-    (({
-        addPost: (post: Post) => {
-            dispatch(addPostToMarketplace(post));
-        },
-    } as unknown) as AddPostScreenProps);
-
-
-export default connect(mapStateToProps, mapDispatchToProps)(AddPostScreen);
+export default withStyles(styles, { withTheme: true })(AddPostScreen);
