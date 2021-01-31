2902ea48ec4803ef00b749c3e3c97cdaef1250fa
jpassgo <jeffpascoe9@gmail.com>
2021-01-30 18:27:25 -0600

    Updating the PostsList component to have each Post be an individual card.

--- junk-trader/src/components/widgets/PostsList.tsx
+++ junk-trader/src/components/widgets/PostsList.tsx
@@ -1,25 +1,57 @@
 import { connect } from 'react-redux';
 import React from 'react';
-import { Grid } from '@material-ui/core';
 import { Dispatch } from 'redux';
+import { Avatar, Card, CardContent, CardHeader, CardMedia, Typography, makeStyles, Theme, createStyles } from '@material-ui/core';
+import SportsEsportsIcon from '@material-ui/icons/SportsEsports';
+import { State } from '../../config/redux/store';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    avatar: {
+      // todo:  using the "theme" for colors
+      background: theme.palette.primary.main,
+    },
+    media: {
+      minHeight: '20vh',
+      background: theme.palette.secondary.main,
+    },
+  })
+);
 
 const PostsList = (props: PostsListProps): JSX.Element => {
+
+        const classes = useStyles();
+
         return( 
         <React.Fragment>
             {props.posts.map((post: Post, index: number) => (
-                <Grid container>
-                    <Grid item xs={4}>
-                        {`Title ${post.title}`}
-                    </Grid>
-                    <br/>
-                    <Grid item xs={4}>
-                        {`Price ${post.price}`}
-                    </Grid>
-                    <br/>
-                    <Grid item xs={4}>
-                        {`Description ${post.description}`}
-                    </Grid>
-                </Grid>
+
+                <Card>
+                    <CardHeader
+                        title={'Card Header'}
+                        subheader={'with some subheader'}
+                        avatar={
+                            <Avatar className={classes.avatar}>
+                                <SportsEsportsIcon />
+                            </Avatar>
+                        }
+                    />
+                    <CardMedia
+                        className={classes.media}
+                        title={'Some Card content title for on hover'}
+                    />
+                    <CardContent>
+                        <Typography>
+                            {`Title ${post.title}`}
+                        </Typography>
+                        <Typography>
+                            {`Price ${post.price}`}
+                        </Typography>
+                        <Typography>
+                            {`Description ${post.description}`}
+                        </Typography>
+                    </CardContent>
+                </Card>            
             ))}
         </React.Fragment>
     );
@@ -29,9 +61,9 @@ export interface PostsListProps {
     posts: Post[]
 }
 
-const mapStateToProps = (state: PostState): PostsListProps => {
+const mapStateToProps = (state: State): PostsListProps => {
     return ({
-        posts: state.posts
+        posts: state.applicationState.posts
     } as unknown) as PostsListProps;
 };
 
