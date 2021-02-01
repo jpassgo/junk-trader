e49beb3a303bc88198e30cecae2747957f5c3f2b
jpassgo <jeffpascoe9@gmail.com>
2021-01-31 18:14:00 -0600

    Replacing some card with a list of default posts.

--- junk-trader/src/components/top-level-components/HomeScreen.tsx
+++ junk-trader/src/components/top-level-components/HomeScreen.tsx
@@ -8,6 +8,7 @@ import {
 } from '@material-ui/core/styles';
 import { Grid } from '@material-ui/core';
 import SomeCard from '../widgets/SomeCard';
+import PostsList from '../widgets/PostsList';
 
 const styles: Styles<Theme, StyledComponentProps> = () => ({});
 
@@ -17,8 +18,11 @@ class HomeScreen extends Component<HomeScreenProps> {
 
     return (
       <Grid container alignItems={'center'} justify={'center'}>
-        <Grid item xs={4}>
+        {/* <Grid item xs={4}>
           <SomeCard />
+        </Grid> */}
+        <Grid item xs={4}>        
+          <PostsList />
         </Grid>
       </Grid>
     );
