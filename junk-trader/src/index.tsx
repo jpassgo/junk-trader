174ca3205cbf0d3d41107132ea65f7f734540af3
jpassgo <jeffpascoe9@gmail.com>
2021-02-15 18:33:07 -0600

    Updating router.

--- junk-trader/src/index.tsx
+++ junk-trader/src/index.tsx
@@ -4,12 +4,16 @@ import ReactDOM from 'react-dom'
 import App from './App';
 import { createHashHistory } from 'history';
 import { ConnectedRouter } from 'connected-react-router';
-import { Route } from 'react-router';
 import { createStore } from './config/redux/store';
 import routes from './config/constants/routes';
 import HomeScreen from './components/top-level-components/HomeScreen';
 import AddPostScreen from './components/top-level-components/AddPostScreen';
 import { init } from './creators/init';
+
+import { Route } from 'react-router';
+
+
+
 
 const history = createHashHistory(),
   store = createStore(history);
@@ -23,17 +27,19 @@ ReactDOM.render(
         {/*// todo:  this is the main "App" component, any component inside gets rendered as the App's "children"*/}
         <App>
           {/*// todo:  below <div /> tag with className={'route'} is important for the router*/}
-          <div className={'route'}>
+          <div className={'route'}>    
+          
             <Route
-              component={HomeScreen}
               exact
+              component={HomeScreen}                
               path={routes.HOME_SCREEN}
             />
             <Route
-              component={AddPostScreen}
               exact
+              component={AddPostScreen}              
               path={routes.ADD_POST_SCREEN}
             />
+                    
           </div>
         </App>
       </ConnectedRouter>
